bda61ec7698e290baef66ae5af63be32e51cbc67
42maxims <42maxims@gmail.com>
2018-08-17 17:31:14 -0400

    Initial site

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Download Hugo
+# Download and extreact Hugo
 wget https://github.com/gohugoio/hugo/releases/download/v0.47/hugo_0.47_Linux-64bit.tar.gz
 tar xzvf hugo_0.47_Linux-64bit.tar.gz hugo
 rm hugo_0.47_Linux-64bit.tar.gz
+
+# Initial site setup
+# ./hugo new site 42maxims
+# cp 42maxims/* . && rmdir 42maxims
+# git clone --depth 1 https://github.com/MunifTanjim/minimo themes/minimo
+# cp theme/minimo/exampleSite/config.toml .
