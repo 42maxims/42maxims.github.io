95818bc18e2e5c96a0b4d766df5ef63f457cf4b6
42maxims <42maxims@gmail.com>
2018-08-17 16:44:57 -0400

    Added hugo setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Download Hugo
+wget https://github.com/gohugoio/hugo/releases/download/v0.47/hugo_0.47_Linux-64bit.tar.gz
+tar xzvf hugo_0.47_Linux-64bit.tar.gz hugo
+rm hugo_0.47_Linux-64bit.tar.gz
